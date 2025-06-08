e1317299a15ec6604f87f16749615e69945db409
nguyentuankiet <ntk28042002@gmail.com>
2025-06-08 21:02:06 +0700

    feat: enhance password validation in RegisterDto by adding MaxLength and Matches decorators for improved security and user feedback

--- src/modules/auth/dto/register.dto.ts
+++ src/modules/auth/dto/register.dto.ts
@@ -5,6 +5,8 @@ import {
   IsEnum,
   IsNotEmpty,
   IsString,
+  Matches,
+  MaxLength,
   MinLength,
 } from 'class-validator';
 
@@ -23,6 +25,14 @@ export class RegisterDto {
   @IsNotEmpty({ message: 'Password is required' })
   @IsString()
   @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  @MaxLength(20, { message: 'Password must be less than 20 characters' })
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    {
+      message:
+        'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
+    },
+  )
   password: string;
 
   @ApiProperty()

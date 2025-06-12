1445f69903ef188d7d8a596a764d6b3749e7d85e
nguyentuankiet <ntk28042002@gmail.com>
2025-06-13 00:17:11 +0700

    feat: enhance role management with DTO validation and unique constraints

--- src/modules/roles/dto/create-role.dto.ts
+++ src/modules/roles/dto/create-role.dto.ts
@@ -1,1 +1,17 @@
-export class CreateRoleDto {}
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { RolesNameEnum } from 'src/enums';
+
+export class CreateRoleDto {
+    @ApiProperty({
+        enum: RolesNameEnum,
+    })
+    @IsNotEmpty({ message: 'Role name is required' })
+    @IsEnum(RolesNameEnum)
+    name: RolesNameEnum;
+
+    @ApiPropertyOptional()
+    @IsOptional()
+    @IsString()
+    description?: string;
+}

1f5bbe70a431d8cc0973e48f957bb523d8fe3b2b
nguyentuankiet <ntk28042002@gmail.com>
2025-06-12 15:30:32 +0700

    refactor: remove BlogServiceRelations module and related files to streamline the codebase

--- src/modules/blog-service-relations/dto/update-blog-service-relation.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateBlogServiceRelationDto } from './create-blog-service-relation.dto';
-
-export class UpdateBlogServiceRelationDto extends PartialType(
-    CreateBlogServiceRelationDto,
-) {}

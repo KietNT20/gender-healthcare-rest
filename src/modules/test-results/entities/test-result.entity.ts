0078156969f9f588745de30eec8824c6e6ffa3ff
nguyentuankiet <ntk28042002@gmail.com>
2025-07-11 12:04:18 +0700

    fix(test-results): remove cascade option from documents relationship in TestResult entity

--- src/modules/test-results/entities/test-result.entity.ts
+++ src/modules/test-results/entities/test-result.entity.ts
@@ -58,9 +58,7 @@ export class TestResult {
     @DeleteDateColumn({ nullable: true })
     deletedAt?: Date;
 
-    @OneToMany(() => Document, (document) => document.testResult, {
-        cascade: true,
-    })
+    @OneToMany(() => Document, (document) => document.testResult)
     documents: Document[];
 
     @OneToOne(() => Appointment, (appointment) => appointment.testResult)
